4ec240316414d032e29c26ec20edbd4f2b944891
ezrafrary <91634826+ezrafrary@users.noreply.github.com>
2025-01-13 14:44:31 -0800

    added a ton of stuff, multiplayer is working, multple waepons can be equiped, added a name system and a join screen, still only one room at a time, workin on that, all weapons still hitscan, also workin on that

--- 1s/Assets/Weapon.cs
+++ 1s/Assets/Weapon.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using Photon.Pun;
+using TMPro;
 
 public class Weapon : MonoBehaviour
 {
@@ -16,19 +17,88 @@ public class Weapon : MonoBehaviour
     [Header("VFX")]
     public GameObject hitVFX;
 
+    [Header("Ammo")]
+    public int mag = 5;
+    public int ammo = 30;
+    public int magAmmo = 30;
+
+    [Header("UI")]
+    public TextMeshProUGUI magText;
+    public TextMeshProUGUI ammoText;
+
+    [Header("Animation")]
+    public Animation animation;
+    public AnimationClip reload;
+
+
+    [Header("Recoil Settings")]
+    [Range(0,1)]
+    // public float recoilPercent = 0.3f;
+    // [Range(0,2)]
+    public float recoverPercent = 0.7f;
+    [Space]
+    public float recoilUp = 1f;
+    public float recoilBack = 0;
+
+
+
+    private Vector3 originalPosition;
+    private Vector3 recoilVelocity = Vector3.zero;
+
+    private bool recoiling;
+    private bool recovering;
+    private float recoilLength;
+    private float recoverLength;
+
+    void Start(){
+        magText.text = mag.ToString();
+        ammoText.text = ammo + "/" + magAmmo;
+        originalPosition = transform.localPosition;
+        recoilLength = 0;
+        recoverLength = 1 / fireRate * recoverPercent;
+    }
+
     void Update()
     {
         if (nextFire > 0){
             nextFire -= Time.deltaTime;
         }
 
-        if (Input.GetButton("Fire1") && nextFire <= 0){
+        if (Input.GetButton("Fire1") && nextFire <= 0 && ammo > 0 && animation.isPlaying == false){
             nextFire = 1 / fireRate;
+            ammo--;
+            
+            magText.text = mag.ToString();
+            ammoText.text = ammo + "/" + magAmmo;
             Fire();
+        }
+
+        if (Input.GetKeyDown(KeyCode.R) && animation.isPlaying == false && mag > 0 && magAmmo > ammo){
+            Reload();
+        }
+
+        if (recoiling){
+            Recoil();
+        }
+        if (recovering){
+            Recovering();
         }
     }
 
+    void Reload(){
+        animation.Play(reload.name);
+
+        if (mag > 0){
+            mag--;
+            ammo = magAmmo;
+        }
+        magText.text = mag.ToString();
+        ammoText.text = ammo + "/" + magAmmo;
+    }
+
     void Fire(){
+        recoiling = true;
+        recovering = false;
         Ray ray = new Ray(camera.transform.position, camera.transform.forward);
 
         RaycastHit hit;
@@ -41,5 +111,23 @@ public class Weapon : MonoBehaviour
         }
     }
 
-   
+    void Recoil(){
+        Vector3 finalPosition = new Vector3(originalPosition.x, originalPosition.y + recoilUp, originalPosition.z - recoilBack);
+        transform.localPosition = Vector3.SmoothDamp(transform.localPosition, finalPosition, ref recoilVelocity, recoilLength);
+
+        if(transform.localPosition == finalPosition){
+            recoiling = false;
+            recovering = true;
+        }
+    }
+    
+    void Recovering(){
+        Vector3 finalPosition = originalPosition;
+        transform.localPosition = Vector3.SmoothDamp(transform.localPosition, finalPosition, ref recoilVelocity, recoverLength);
+
+        if(transform.localPosition == finalPosition){
+            recoiling = false;
+            recovering = false;
+        }
+    }
 }
